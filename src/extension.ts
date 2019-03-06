543a809cefbca786d73d8c8ba8fc0cbe79755bcd
jenchieh <jcs090218@gmail.com>
2019-03-06 19:18:39 +0800

    Test.

--- src/extension.ts
+++ src/extension.ts
@@ -9,39 +9,40 @@ import { vscode_api } from "./vscode_api";
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-    let testCommands = vscode.commands.registerCommand('vscode-api.testStatus', () => {
-        let editor = vscode_api.getActiveTextEditor();
-        if (!editor) {
-            return;
-        }
+    let testCommands = vscode.commands.registerCommand(
+        'vscode-api.testStatus',
+        () => {
+            let editor = vscode_api.getActiveTextEditor();
+            if (!editor) {
+                return;
+            }
 
-        console.log("----------------- Start VSCode API Test -----------------");
+            console.log("----------------- Start VSCode API Test -----------------");
 
-        console.log("Is beginnnig of line? " + vscode_api.isBeginningOfLine(editor));
-        console.log("Is end of line? " + vscode_api.isEndOfLine(editor));
-        console.log("Is beginnnig of buffer? " + vscode_api.isBeginningOfBuffer(editor));
-        //console.log("Is end of buffer? " + vscode_api.isEndOfBuffer(editor));
+            console.log("Is beginnnig of line? " + vscode_api.isBeginningOfLine(editor));
+            console.log("Is end of line? " + vscode_api.isEndOfLine(editor));
+            console.log("Is beginnnig of buffer? " + vscode_api.isBeginningOfBuffer(editor));
+            console.log("Is end of buffer? " + vscode_api.isEndOfBuffer(editor));
 
-        console.log("Current line number: " + vscode_api.currentLine(editor));
-        console.log("Current column: " + vscode_api.currentColumn(editor));
+            console.log("Current line number: " + vscode_api.currentLine(editor));
+            console.log("Current column: " + vscode_api.currentColumn(editor));
 
-        console.log("----------------- End VSCode API Test -----------------");
-    });
+            console.log("----------------- End VSCode API Test -----------------");
+        });
 
-    let testStatus = vscode.commands.registerCommand('vscode-api.testCommands', () => {
-        let editor = vscode_api.getActiveTextEditor();
-        if (!editor) {
-            return;
-        }
+    let testStatus = vscode.commands.registerCommand(
+        'vscode-api.testCommands',
+        () => {
+            let editor = vscode_api.getActiveTextEditor();
+            if (!editor) {
+                return;
+            }
 
-        //vscode_api.toPoint(editor, 7, 51);
-        //vscode_api.backwardChar(editor);
-        //vscode_api.findEndColumn(editor);
-        //vscode_api.findEndColumn(editor);
+            vscode_api.nextLine(editor);
+        });
 
-        vscode_api.endOfLine(editor);
-    });
 
+    registerVSCodeAPI(context);
 
     context.subscriptions.push(testCommands);
     context.subscriptions.push(testStatus);
@@ -49,3 +50,35 @@ export function activate(context: vscode.ExtensionContext) {
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
+
+
+/**
+ * @desc Register all the vscode api to test.
+ * @param { vscode.ExtensionContext } context : extension context.
+ */
+function registerVSCodeAPI(context : vscode.ExtensionContext) {
+
+    let nextLine = vscode.commands.registerCommand(
+        'vscode-api.nextLine',
+        () => {
+            let editor = vscode_api.getActiveTextEditor();
+            if (!editor) {
+                return;
+            }
+            vscode_api.nextLine(editor);
+        });
+
+    let previousLine = vscode.commands.registerCommand(
+        'vscode-api.previousLine',
+        () => {
+            let editor = vscode_api.getActiveTextEditor();
+            if (!editor) {
+                return;
+            }
+            vscode_api.previousLine(editor);
+        });
+
+
+    context.subscriptions.push(nextLine);
+    context.subscriptions.push(previousLine);
+}
